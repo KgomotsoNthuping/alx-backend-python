ad350046bfb60bfebd7dc6361a06db140aa05f22
Kgomotso Nthuping <138695830+KgomotsoNthuping@users.noreply.github.com>
2024-06-10 20:46:53 +0200

    pyt

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -4,6 +4,7 @@ import asyncio
 from typing import List
 task_wait_random = __import__('3-tasks').task_wait_random
 
+
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """creating and running multiple asyncio tasks"""
     r = await asyncio.gather(
